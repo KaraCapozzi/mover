0dee8457421d322158b620cff7852670d1a5c619
sakin <egalsakin@gmail.com>
2018-02-20 17:27:10 -0500

    add user model fullname, validate fullname

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131192219) do
+ActiveRecord::Schema.define(version: 20180220221108) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
