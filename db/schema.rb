41d5c03ef4038d7efea9d90401b13f86d0766d34
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2018-02-21 17:07:15 -0500

    fullname added for user, avatar visible

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180220221108) do
-=======
 ActiveRecord::Schema.define(version: 20180221183507) do
->>>>>>> styling-navbar
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
